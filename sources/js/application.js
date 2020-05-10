5c0a143863430e187dc94e57180233d59be0dc39
ericgaspar <junk.eg@free.fr>
2020-05-10 11:52:46 +0200

    sources

---
+++ sources/js/application.js
@@ -0,0 +1,3 @@
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  new GameManager(5, KeyboardInputManager, HTMLActuator, LocalScoreManager);});
